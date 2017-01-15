1db8526873d65b8b02e27c491676d7d4d590367b
ToumaKazusa <gaofc410@gmail.com>
2017-01-14 16:47:01 -0800

    Code

--- Assignment1/Logistic.py
+++ Assignment1/Logistic.py
@@ -11,25 +11,20 @@ train = np.concatenate((train.T, ones), axis = 0).T
 
 weight = np.ones((785, 1))
 for i in range(0, 785):
- 	weight[i] = 0.001
-
-sums = np.zeros((785, 1))
-for i in range(0, 20000):
-	if labels[i] == 2 | labels[i] == 3:
-		for j in range(0, 785):
-			sums[j] += 1
-
+ 	weight[i] = 0.01
 
 for loop in range(0, 1000):
 	y = train.dot(weight)
-	grad = 0
+	err = 0
 	for i in range(0, 20000):
 		if labels[i] == 2:
-			grad += 1 - y[i]
+			err += 1 - y[i]
+			for j in range(0, 785):
+				weight[j] += 0.001 * (y[i] - 1) * train[i][j]
 		if labels[i] == 3:
-			grad += -1 - y[i]
-	for i in range(0, 785):
-		weight[i] += 0.000000001*grad
-	print(grad)
+			err += - 1 - y[i]
+			for j in range(0, 785):
+				weight[j] += 0.001 * y[i] * train[i][j]
+	print(err)
 
 
